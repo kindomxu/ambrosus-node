079619b7090f41ac6866bf1def4bb920da0e543d
Dani Hodovic <dani.hodovic@gmail.com>
2018-11-21 18:35:24 +0300

    Hermes / Atlas worker health checks

--- src/workers/atlas_worker.js
+++ src/workers/atlas_worker.js
@@ -7,12 +7,25 @@ This Source Code Form is subject to the terms of the Mozilla Public License, v. 
 This Source Code Form is “Incompatible With Secondary Licenses”, as defined by the Mozilla Public License, v. 2.0.
 */
 
+import express from 'express';
+import asyncMiddleware from '../middlewares/async_middleware';
+import healthCheckHandler from '../routes/health_check';
 import PeriodicWorker from './periodic_worker';
 import AtlasChallengeParticipationStrategy from './atlas_strategies/atlas_challenge_resolution_strategy';
 import {getTimestamp} from '../utils/time_utils';
 
 export default class AtlasWorker extends PeriodicWorker {
-  constructor(web3, dataModelEngine, workerLogRepository, challengesRepository, failedChallengesCache, strategy, logger) {
+  constructor(
+    web3,
+    dataModelEngine,
+    workerLogRepository,
+    challengesRepository,
+    failedChallengesCache,
+    strategy,
+    logger,
+    mongoClient,
+    serverPort
+  ) {
     super(strategy.workerInterval, logger);
     this.web3 = web3;
     this.dataModelEngine = dataModelEngine;
@@ -20,6 +33,13 @@ export default class AtlasWorker extends PeriodicWorker {
     this.workerLogRepository = workerLogRepository;
     this.challengesRepository = challengesRepository;
     this.failedChallengesCache = failedChallengesCache;
+    this.mongoClient = mongoClient;
+    this.expressApp = express();
+    this.serverPort = serverPort;
+    this.expressApp.get('/health', asyncMiddleware(
+      healthCheckHandler(mongoClient, web3)
+    ));
+
     if (!(this.strategy instanceof AtlasChallengeParticipationStrategy)) {
       throw new Error('A valid strategy must be provided');
     }
@@ -79,5 +99,14 @@ export default class AtlasWorker extends PeriodicWorker {
     };
     this.logger.info({...log, stacktrace});
     await this.workerLogRepository.storeLog({timestamp: getTimestamp(), ...log});
+  }
+
+  beforeWorkLoop() {
+    this.server = this.expressApp.listen(this.serverPort);
+  }
+
+  async afterWorkLoop() {
+    await this.server.close();
+    await this.mongoClient.close();
   }
 }
